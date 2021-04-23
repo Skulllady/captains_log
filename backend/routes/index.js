7297d054efbfb71d5e1f28c6d52eba7018d4f474
Skulllady <noorhoque45@gmail.com>
2021-04-23 14:59:57 -0400

    back-end setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.get('/hello/world', function (req, res) {
-  
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function (req, res) {
+
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
 router.use('/api', apiRouter);
 
