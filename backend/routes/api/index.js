06f5310dc62ddf464783f6f4e6d6a8a68ce12df6
Skulllady <noorhoque45@gmail.com>
2021-04-23 14:19:15 -0400

    phase4 login route created and tested

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,10 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
