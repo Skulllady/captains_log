148ad1dc96ee0aa71306c9108eff73dd1543d99e
Skulllady <noorhoque45@gmail.com>
2021-04-22 23:38:59 -0400

    error handling completed

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const routes = require('./routes');
@@ -23,7 +24,7 @@ if (!isProduction) {
 app.use(helmet({
   contentSecurityPolicy: false
 }));
-console.log("CCCSSSURFFFF", csurf);
+
 // Set the _csrf token and create req.csrfToken method
 app.use(
   csurf({
@@ -35,5 +36,36 @@ app.use(
   })
 );
 app.use(routes); // Connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
