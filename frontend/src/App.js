a638dba9aa125f6246ffa10f40633ce0a338b77c
Skulllady <noorhoque45@gmail.com>
2021-04-28 01:37:53 -0400

    added sidebar component, which is only rendered when user is logged in and also tested demo user and logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import LoginFormPage from './components/LoginFormPage'
+import SideBar from './components/SideBar'
 
 function App() {
   const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user)
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  //check if user is logged in using sessionUser
   return (
     <>
-      <Navigation isLoaded={isLoaded} />
+      {sessionUser ? <SideBar /> :
+        <Navigation isLoaded={isLoaded} />
+      }
       {isLoaded && (
         <Switch>
           <Route path="/signup">
---
+++ frontend/src/components/SideBar/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './SideBar.css';
+
+function SideBar() {
+  return (
+    <div>SIDEBAR</div>
+  )
+}
+
+
+
+export default SideBar;
